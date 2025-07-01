8581287a42693fc4c8ce8308465a057e1f9fe360
Rakesh Govinda Subramanyam <govindasubramanyamrakesh1999@gmail.com>
2025-07-01 12:40:32 -0400

    update index.html

--- test.py
+++ test.py
@@ -2,3 +2,9 @@ print("Hello, World!")
 
 #Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
 #Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned
+
+
+Designed and implemented ETL processes using complex SQL queries, stored procedures, and Azure Data Factory to support data integration and transformation across systems.
+Automated SQL-based data validation, backup/restore, and schema deployment tasks using PowerShell, Azure CLI, and CI/CD pipelines in Azure DevOps.
+Developed monitoring and alerting dashboards using Azure Monitor, Log Analytics, and SQL performance insights to   proactively resolve issues and optimize database health.
+Tools Used: Azure, SSMS, SQL Server, Azure Data Factory, PowerShell, Azure CLI, Azure DevOps. 
